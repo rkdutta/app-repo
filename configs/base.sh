2f365b21389f1406b28189a6aa4e26e79d16c982
Rajesh Dutta <rdutta@Rajeshs-MacBook-Pro.local>
2023-01-02 19:38:01 +0100

    added patch on ks file

---
+++ configs/base.sh
@@ -0,0 +1,38 @@
+#!/bin/bash  
+
+# base folder
+basePath="$1"
+tenant="$2"
+environment="$3"
+
+if [[ -z "$basePath" ]]; then
+ echo "missing input basepath"
+ exit 1;
+fi
+
+if [[ -z "$tenant" ]]; then
+ echo "missing input tenant"
+ exit 1;
+fi
+
+if [[ -z "$environment" ]]; then
+ echo "missing input environment"
+ exit 1;
+fi
+
+cd $basePath
+rm -f kustomization.yaml
+kustomize init 
+
+for file in *; 
+do 
+    echo "Processing $file file..."
+    
+    fileNameWithOutPath=`basename $file`
+    fileNameExtn=${fileNameWithOutPath##*.}
+    fileNameWithOutPathAndExtn=`basename $file .$fileNameExtn`
+    kustomize edit add configmap $fileNameWithOutPathAndExtn --behavior=create --from-file=$fileNameWithOutPath
+done
+
+cd ../tenants/$tenant/$environment
+pwd
--- configs/run.sh
+++ configs/run.sh
@@ -3,14 +3,40 @@ tenant=$1
 basePath="defaults"
 env=$2
 
-rm -rfd merged-*
+rm -rfd tenants/$tenant/merged-*
+mkdir -p tenants/$tenant/merged-$env
+kustomize create
+kustomize edit set namespace $tenant-$env
 
 for file in tenants/$tenant/$env/*.yaml; 
 do 
-    echo;echo;
     echo "Processing $file file..."
-    mkdir -p tenants/$tenant/merged-$env
-    file=$file yq ea '. *= load(env(file))' "$basePath/`basename $file`" > tenants/$tenant/merged-$env/`basename $file`
-
+    
+    fileNameWithOutPath=`basename $file`
+    fileNameWithOutPathAndExtn=`basename $file .yaml`
+    
+    file=$file yq ea '. *= load(env(file))' "$basePath/$fileNameWithOutPath" > tenants/$tenant/merged-$env/$fileNameWithOutPath
+    kustomize edit add configmap $fileNameWithOutPathAndExtn --behavior=create --from-file=tenants/$tenant/merged-$env/$fileNameWithOutPath
 done
+
+mv kustomization.yaml tenants/$tenant/merged-$env
+
 # yq ea '. *= load("tenants/kla/$env/podinfo-values.yaml")' $basePath/podinfo-values.yaml
+
+
+# base folder
+basePath="base"
+extn="yaml"
+cd $basePath
+rm -f kustomization.yaml
+
+kustomize create 
+
+for file in *.$extn; 
+do 
+    echo "Processing $file file..."
+    
+    #fileNameWithOutPath=`basename $file`
+    #fileNameWithOutPathAndExtn=`basename $file .$extn`
+    #kustomize edit add configmap $fileNameWithOutPathAndExtn --behavior=create --from-file=$fileNameWithOutPath
+done
