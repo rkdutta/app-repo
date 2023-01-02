19516bc40ae93ffecd55c255eac5d8ed9bd75d41
Rajesh Dutta <rdutta@Rajeshs-MacBook-Pro.local>
2023-01-02 17:55:05 +0100

    added patch on ks file

--- configs/run.sh
+++ configs/run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash  
-tenant="kla"
+tenant=$1
 basePath="defaults"
-env="acc"
+env=$2
+
+rm -rfd merged-*
 
 for file in tenants/$tenant/$env/*.yaml; 
 do 
     echo;echo;
     echo "Processing $file file..."
-    file=$file yq ea '. *= load(env(file))' "$basePath/`basename $file`"
+    mkdir -p tenants/$tenant/merged-$env
+    file=$file yq ea '. *= load(env(file))' "$basePath/`basename $file`" > tenants/$tenant/merged-$env/`basename $file`
 
 done
 # yq ea '. *= load("tenants/kla/$env/podinfo-values.yaml")' $basePath/podinfo-values.yaml
