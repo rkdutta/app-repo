84892ccb796693575c9acb5ffd2a0278f0b1b770
Rajesh Dutta <rdutta@Rajeshs-MacBook-Pro.local>
2023-01-08 01:01:33 +0100

    updated

--- deploy/configs/base.sh
+++ deploy/configs/base.sh
@@ -58,35 +58,3 @@ for dir in */ ; do
         fi
     done
 done
-
-
-# # for tenants
-
-# cd ../tenants/$tenant/$environment
-# rm -f kustomization.yaml
-# kustomize init
-# kustomize edit add resource "../../../base"
-
-# kustomize edit set namespace $tenant-$environment
-
-# for dir in */ ; do
-#     echo "Processing in $dir"
-
-#     for file in $dir*; 
-#     do 
-#         echo "Processing $file file..."
-        
-#         fileNameWithOutPath=`basename $file`
-#         fileNameExtn=${fileNameWithOutPath##*.}
-#         fileNameWithOutPathAndExtn=`basename $file .$fileNameExtn`
-   
-#     if [[ $dir == "files/" ]]; then
-#         kustomize edit add configmap $fileNameWithOutPathAndExtn --behavior=merge --from-file=$file
-#     elif [[ $dir == "envs/" ]]; then
-#         kustomize edit add configmap $fileNameWithOutPathAndExtn --behavior=merge --from-env-file=$file
-#     else
-#         echo "Unsupported directory structure -  $dir"
-#         exit 1
-#     fi
-#     done
-# done
