66577bc5af00359c6040cd04e9fcb483905f3fe4
Rajesh Dutta <rdutta@Rajeshs-MacBook-Pro.local>
2023-01-08 01:27:23 +0100

    updated

--- deploy/configs/base.sh
+++ deploy/configs/base.sh
@@ -2,15 +2,17 @@
 
 # for base 
 
-echo "syntax: ./base.sh runFor[base or tenant] basePath[base/ or tenants/] tenant[kla] environment[acc or dev]"
+echo "syntax: ./base.sh runFor[base or variant or tenant] basePath[base/ or tenants/] tenant[kla] environment[acc or dev]"
 
 runFor=$1 # base or tenant
 basePath="$2"
-tenant="$3"
-environment="$4"
+environment="$3"
+updateStratey=$4
+tenant="$5"
+
 
 if [[ -z "$runFor" ]]; then
- echo "missing input runFor [base or tenant]"
+ echo "missing input runFor [base or variant or tenant]"
  exit 1;
 fi
 
@@ -24,11 +26,19 @@ if [[ "$runFor" == "base" ]]; then
     cd $basePath
     rm -f kustomization.yaml
     kustomize init
+elif [[ "$runFor" == "variant" && "$environment" != "" ]]; then 
+    cd $basePath/$environment
+    pwd
+    rm -f kustomization.yaml
+    kustomize init
+    kustomize edit add resource "../../base"
+    kustomize edit set namespace $environment
 elif [[ "$runFor" == "tenant" && "$environment" != "" ]]; then 
     cd $basePath/$tenant/$environment
     rm -f kustomization.yaml
     kustomize init
     kustomize edit add resource "../../../base"
+    kustomize edit add component "../../../variants/$environment"
     kustomize edit set namespace $tenant-$environment
 else
     echo "Unsupported -  $runFor / $basePath / $environment"
@@ -47,11 +57,11 @@ for dir in */ ; do
         fileNameWithOutPathAndExtn=`basename $file .$fileNameExtn`
    
         if [[ $dir == "envs/" ]]; then
-            kustomize edit add configmap $fileNameWithOutPathAndExtn --behavior=create --from-env-file=$file --disableNameSuffixHash
+            kustomize edit add configmap $fileNameWithOutPathAndExtn --behavior=$updateStratey --from-env-file=$file --disableNameSuffixHash
         elif [[ $dir == "files/" ]]; then
-            kustomize edit add configmap $fileNameWithOutPathAndExtn --behavior=create --from-file=$file --disableNameSuffixHash
+            kustomize edit add configmap $fileNameWithOutPathAndExtn --behavior=$updateStratey --from-file=$file --disableNameSuffixHash
         elif [[ $dir == "values/" ]]; then
-            kustomize edit add configmap $fileNameWithOutPathAndExtn --behavior=create --from-file=values.yaml=$file --disableNameSuffixHash
+            kustomize edit add configmap $fileNameWithOutPathAndExtn --behavior=$updateStratey --from-file=values.yaml=$file --disableNameSuffixHash
         else
             echo "Unsupported directory structure -  $dir"
             exit 1
